e7a4487542cc759bdc70bb1a2d379550a8e5ece7
Taha HALIM <mohamedtahahalim@gmail.com>
2025-07-13 20:04:39 +0100

    Refactored useFetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 import type { bookingDetails } from "../utils/types";
 
-export default function useFetch() {
-  const url = import.meta.env.VITE_API_URL + `/api/getBookings`;
+interface IUseFetch {
+  contents: bookingDetails[];
+  error: string;
+  loading: boolean;
+  filteredContent: bookingDetails[];
+  setFilteredContent: React.Dispatch<React.SetStateAction<bookingDetails[]>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
+export default function useFetch(): IUseFetch {
   const [contents, setContents] = useState<bookingDetails[]>([]);
   const [filteredContent, setFilteredContent] = useState<bookingDetails[]>([]);
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
+  const url =
+    (import.meta.env.VITE_API_URL || "http://localhost:3000") +
+    `/api/getBookings`;
 
   useEffect(() => {
     (async () => {
@@ -15,18 +25,15 @@ export default function useFetch() {
       setLoading(true);
       try {
         const response = await fetch(url, { signal: controller.signal });
+        if (!response.ok) {
+          console.log(
+            "Error fetching data from backend with a status error of : ",
+            response.status
+          );
+          return;
+        }
         const data = await response.json();
         setContents(
-          data.bookings.map((element: any) => {
-            return {
-              ...element,
-              images: JSON.parse(element.images),
-              amenities: JSON.parse(element.amenities),
-              jp_amenities: JSON.parse(element.jp_amenities),
-            };
-          })
-        );
-        setFilteredContent(
           data.bookings.map((element: any) => {
             return {
               ...element,
@@ -46,6 +53,10 @@ export default function useFetch() {
       };
     })();
   }, []);
+
+  useEffect(() => {
+    setContents({ ...contents });
+  }, [contents]);
 
   return {
     contents,
